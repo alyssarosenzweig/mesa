a69d6288eb655c82be99bfb7f56861b0d2192842
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2022-05-21 22:12:49 -0400

    agx: Call opt_shrink_vectors
    we benefit form loading fewer components. Noticed on a gfxbench shader.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>

--- src/asahi/compiler/agx_compile.c
+++ src/asahi/compiler/agx_compile.c
@@ -1477,6 +1477,7 @@ agx_optimize_nir(nir_shader *nir)
       NIR_PASS(progress, nir, nir_opt_undef);
       NIR_PASS(progress, nir, nir_lower_undef_to_zero);
 
+      NIR_PASS(progress, nir, nir_opt_shrink_vectors);
       NIR_PASS(progress, nir, nir_opt_loop_unroll);
    } while (progress);
 
